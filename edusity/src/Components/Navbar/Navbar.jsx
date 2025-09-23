9c8e181f2672e47ab003d2892c3789df991f53de
Ayush-Numentica <ayush.ranjan@numentica-ui.com>
2025-09-23 15:33:16 +0530

    modified:   edusity/package-lock.json 	modified:   edusity/package.json 	modified:   edusity/src/App.jsx 	new file:   edusity/src/Components/Contact/Contact.css 	new file:   edusity/src/Components/Contact/Contact.jsx 	new file:   edusity/src/Components/Footer/Footer.css 	new file:   edusity/src/Components/Footer/Footer.jsx 	modified:   edusity/src/Components/Navbar/Navbar.jsx 	new file:   passwordGenerator/.gitignore 	new file:   passwordGenerator/README.md 	new file:   passwordGenerator/eslint.config.js 	new file:   passwordGenerator/index.html 	new file:   passwordGenerator/package-lock.json 	new file:   passwordGenerator/package.json 	new file:   passwordGenerator/src/App.css 	new file:   passwordGenerator/src/App.jsx 	new file:   passwordGenerator/src/index.css 	new file:   passwordGenerator/src/main.jsx 	new file:   passwordGenerator/vite.config.js

--- edusity/src/Components/Navbar/Navbar.jsx
+++ edusity/src/Components/Navbar/Navbar.jsx
@@ -17,11 +17,11 @@ const Navbar = () => {
     <nav className={`container ${sticky ? 'dark-nav':""}`}>
         <img src={logo} alt="" className='logo' />
         <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">Program</a></li>
-            <li><a href="#about">About us</a></li>
-            <li><a href="">Campus</a></li>
-            <li><a href="">Testimonials</a></li>
+            <li>About</li>
+            <li>Program</li>
+            <li>About us</li>
+            <li>Campus</li>
+            <li>Testimonials</li>
             <li><button className='btn'>Contact Us</button></li>
         </ul>
     </nav>
