84953b023c9a5c526f6a6819bacb4576da90e9b7
Ayush-Numentica <ayush.ranjan@numentica-ui.com>
2025-10-03 17:32:42 +0530

    new file:   moviePlatform/.gitignore 	new file:   moviePlatform/README.md 	new file:   moviePlatform/eslint.config.js 	new file:   moviePlatform/index.html 	new file:   moviePlatform/package-lock.json 	new file:   moviePlatform/package.json 	new file:   moviePlatform/public/vite.svg 	new file:   moviePlatform/src/App.jsx 	new file:   moviePlatform/src/assets/react.svg 	new file:   moviePlatform/src/components/MovieCard.jsx 	new file:   moviePlatform/src/components/NavBar.jsx 	new file:   moviePlatform/src/css/App.css 	new file:   moviePlatform/src/css/Favorites.css 	new file:   moviePlatform/src/css/Home.css 	new file:   moviePlatform/src/css/MovieCard.css 	new file:   moviePlatform/src/css/NavBar.css 	new file:   moviePlatform/src/css/index.css 	new file:   moviePlatform/src/main.jsx 	new file:   moviePlatform/src/pages/Favorites.jsx 	new file:   moviePlatform/src/pages/Home.jsx 	new file:   moviePlatform/vite.config.js

---
+++ moviePlatform/src/pages/Favorites.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../css/Favorites.css"
+
+const Favorites = () => {
+  return (
+    <div className='favorites-empty'>
+
+        <h2>No Favorite Movies Yet</h2>
+        <p>Start adding movies to your favorites and the will appear here</p>
+      
+    </div>
+  )
+}
+
+export default Favorites
